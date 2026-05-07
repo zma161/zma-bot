dd25e2455cbb1e36869cd39cba65010b123e1d33
zma161 <mmti-moy@yandex.ru>
2026-05-07 15:43:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ async function setNativeTelegramMiniAppButton(chatId) {
 async function sendSuccessMessage(chatId) {
   await tgApi('sendMessage', {
     chat_id: String(chatId),
-    text: '💜 Личный кабинет теперь доступен через кнопку в нижнем левом углу.'
+    text: '💜 Платформа магазина теперь доступна через кнопку в нижнем левом углу.'
   });
 }
 
